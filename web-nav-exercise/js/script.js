9c10ae6e82c52e844b340b1660ae62a99efa33f0
Marcin Węglorz <marcinweglorz.dev@gmail.com>
2023-01-16 22:20:20 +0100

    fixed code structure

--- web-nav-exercise/js/script.js
+++
@@ -1,6 +0,0 @@
-const mobileNav = document.querySelector('.navigation-list')
-const burgerBtn = document.querySelector('.burger')
-
-burgerBtn.addEventListener('click', function () {
-	mobileNav.classList.toggle('active')
-})
