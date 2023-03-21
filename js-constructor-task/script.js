ce5fbf742c8180e5c9dc81ff901b6a44fb3436ca
Marcin Węglorz <marcinweglorz.dev@gmail.com>
2023-03-21 15:14:27 +0100

    quick constructor js task

---
+++ js-constructor-task/script.js
@@ -0,0 +1,36 @@
+function Person(name, age) {
+	this.name = name
+	this.age = age
+}
+
+Persone.prototype.sayHi = function () {
+	console.log(`Cześć, jestem ${this.name}.`)
+}
+
+Persone.prototype.showAge = function () {
+	console.log(`Mam ${this.age} lat.`)
+}
+
+const person1 = new Person('Marcin', 27)
+person1.sayHi()
+person1.showAge()
+
+// ====================
+
+class Person2 {
+	constructor(name, age) {
+		this.name = name
+		this.age = age
+	}
+
+	sayHi() {
+		console.log(`Cześć, jestem ${this.name}!`)
+	}
+
+	showAge() {
+		console.log(`Mam ${this.age} lat!`)
+	}
+}
+
+const person2 = new Person2('Marcin', 27)
+console.log(person2)
